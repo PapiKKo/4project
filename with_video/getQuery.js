23aa562b7d22e33b63d6429fe5bca2a86bded5aa
PapiKKo <65007528+PapiKKo@users.noreply.github.com>
2021-11-12 15:34:23 +0900

    BGMだけのリンクに変更

--- with_video/getQuery.js
+++ with_video/getQuery.js
@@ -1,5 +1,5 @@
 function execute() {
-    var yoga = document.sampleForm.menu[0];
+    //var yoga = document.sampleForm.menu[0];
     var bgm = document.sampleForm.menu[1];
     var result = document.getElementById("result");
     result.innerHTML = "";
@@ -8,17 +8,9 @@ function execute() {
     const inputValue = textbox.value;
     const url = new URL(inputValue);
     const params = new URLSearchParams(url.search);
-
-    if(yoga.checked) {
-        result.innerHTML = yoga.value + "が選択されています。";
-	window.open("https://papikko.github.io/srt.js/index.htm?"+params+"&surl=./sample/example2_srt.js",'_blank');
-        none = false;
-    }
-    if(bgm.checked) {
-        result.innerHTML += bgm.value + "が選択されています。";
+	result.innerHTML += bgm.value + "が選択されています。";
 	window.open("https://papikko.github.io/srt.js/index.htm?"+params+"&surl=./sample/example3_srt.js",'_blank');
-        none = false;
-    }
+	none = false;
 }
 function btnCopy() {
     const txt = document.querySelector('#ulID').innerText;
