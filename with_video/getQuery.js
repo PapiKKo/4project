56efff545b75cc2940a3170b68349e2081998521
PapiKKo <65007528+PapiKKo@users.noreply.github.com>
2021-11-12 15:52:55 +0900

    Update getQuery.js

--- with_video/getQuery.js
+++ with_video/getQuery.js
@@ -1,6 +1,6 @@
 function execute() {
     //var yoga = document.sampleForm.menu[0];
-    var bgm = document.sampleForm.menu[1];
+    //var bgm = document.sampleForm.menu[0];
     var result = document.getElementById("result");
     result.innerHTML = "";
     var none = true;
@@ -8,11 +8,7 @@ function execute() {
     const inputValue = textbox.value;
     const url = new URL(inputValue);
     const params = new URLSearchParams(url.search);
-	result.innerHTML += bgm.value + "が選択されています。";
+	//result.innerHTML += bgm.value + "が選択されています。";
 	window.open("https://papikko.github.io/4project/index.htm?"+params+"&surl=./sample/example3_srt.js",'_blank');
 	none = false;
-}
-function btnCopy() {
-    const txt = document.querySelector('#ulID').innerText;
-    navigator.clipboard.writeText(txt)
 }
